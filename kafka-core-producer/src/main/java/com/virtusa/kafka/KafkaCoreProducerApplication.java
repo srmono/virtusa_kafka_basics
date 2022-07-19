bb60445f08332976900d6a67a0a90810116db253
srmono <bvsrao91@gmail.com>
2022-07-19 20:29:45 +0530

    multiple consumers, partitions, json usage updated

--- kafka-core-producer/src/main/java/com/virtusa/kafka/KafkaCoreProducerApplication.java
+++ kafka-core-producer/src/main/java/com/virtusa/kafka/KafkaCoreProducerApplication.java
@@ -1,5 +1,6 @@
 package com.virtusa.kafka;
 
+import java.time.LocalDate;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.virtusa.kafka.entity.Employee;
+import com.virtusa.kafka.producer.Employee2JsonProducer;
+import com.virtusa.kafka.producer.EmployeeJsonProducer;
 import com.virtusa.kafka.producer.HelloKafkaProducer;
 import com.virtusa.kafka.producer.KafkaKeyProducer;
 
@@ -18,8 +22,14 @@ public class KafkaCoreProducerApplication implements CommandLineRunner {
 //	@Autowired
 //	private HelloKafkaProducer producer;
 	
+//	@Autowired
+//	private KafkaKeyProducer producer;
+	
+//	@Autowired
+//	private EmployeeJsonProducer producer;
+	
 	@Autowired
-	private KafkaKeyProducer producer;
+	private Employee2JsonProducer producer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaCoreProducerApplication.class, args);
@@ -30,11 +40,19 @@ public class KafkaCoreProducerApplication implements CommandLineRunner {
 		// TODO Auto-generated method stub
 		//producer.sendHello("Virtusa " + ThreadLocalRandom.current().nextInt());
 		
-		for(int i = 0; i < 30; i++) {
-			var key = "Key-" + (i % 4);
-			var value = "value " + i + " with key " + key;
-			producer.send(key, value);
+		// generate message with key
+//		for(int i = 0; i < 30; i++) {
+//			var key = "Key-" + (i % 4);
+//			var value = "value " + i + " with key " + key;
+//			producer.send(key, value);
+//		}
+		
+		for(int i = 0; i < 5; i++ ) {
+			var emp = new Employee("emp" + i, "Employee " + i, LocalDate.now());
+			producer.sendMessage(emp);
 		}
+		
+		
 	}
 
 }
